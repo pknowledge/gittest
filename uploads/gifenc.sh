2bf672be7d5b6ecb9420d90a0de6c97c27d8760d
pknowledge <yogesh.patel88@gmail.com>
2016-09-08 12:33:37 +0200

    uploads/gifenc.sh

--- uploads/gifenc.sh
+++ uploads/gifenc.sh
@@ -4,5 +4,5 @@ palette="/tmp/palette.png"
 #./gifenc.sh SampleVideo_1280x720_1mb.mp4 output.gif 720 10
 filters="fps=$4,scale=$3:-1:flags=lanczos"
 
-./app/uploads/ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
-./app/uploads/ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+/app/uploads/ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+/app/uploads/ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
